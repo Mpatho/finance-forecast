e395033fbec77b4755ba212f439b1b2608ab7034
Mpatho Zulu <zmpatho@gmail.com>
2018-06-13 11:13:46 +0200

    updated entity event

--- src/main/java/psybergate/grad2018/javafnds/finance/entity/Event.java
+++ src/main/java/psybergate/grad2018/javafnds/finance/entity/Event.java
@@ -9,11 +9,50 @@ public class Event {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private EventType type;
-	
+
 	private Integer month;
-	
+
 	private Double value;
+
+	protected Event() {
+
+	}
+
+	public Event(EventType type, Integer month, Double value) {
+		super();
+		this.type = type;
+		this.month = month;
+		this.value = value;
+	}
+
+	public EventType getType() {
+		return type;
+	}
+
+	public void setType(EventType type) {
+		this.type = type;
+	}
+
+	public Integer getMonth() {
+		return month;
+	}
+
+	public void setMonth(Integer month) {
+		this.month = month;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public Long getId() {
+		return id;
+	}
 
 }
