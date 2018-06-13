5f4988e23160d12fc637e037d4d4d5c9cc6c1815
Mpatho Zulu <zmpatho@gmail.com>
2018-06-13 11:23:52 +0200

    resource updated

--- src/main/java/psybergate/grad2018/javafnds/finance/resource/Resource.java
+++ src/main/java/psybergate/grad2018/javafnds/finance/resource/Resource.java
@@ -1,5 +1,16 @@
 package psybergate.grad2018.javafnds.finance.resource;
 
+import java.util.Collection;
+
 public interface Resource<T> {
 
+	T getById(Long id);
+
+	void save(T entity);
+
+	Collection<T> getAll();
+
+	void removeById(Long id);
+
+	void remove(T entity);
 }
