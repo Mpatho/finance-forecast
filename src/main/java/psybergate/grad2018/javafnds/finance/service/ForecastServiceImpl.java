087fdca39a4a2332ee0c4b4751ff53871e38f755
Mahlori Ngobeni <mahlori@psybergate.co.za>
2018-06-14 10:15:19 +0200

    commit removing non javadocs

--- src/main/java/psybergate/grad2018/javafnds/finance/service/ForecastServiceImpl.java
+++ src/main/java/psybergate/grad2018/javafnds/finance/service/ForecastServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.List;
 import javax.ejb.Stateful;
 
 @Stateful
-public abstract class ForecastServiceImpl implements ForecastService{
+public abstract class ForecastServiceImpl implements ForecastService {
 
 	private BigDecimal initialAmount;
 
@@ -14,55 +14,34 @@ public abstract class ForecastServiceImpl implements ForecastService{
 
 	private Integer months;
 
-	/* (non-Javadoc)
-	 * @see psybergate.grad2018.javafnds.finance.service.ForecastService#getForecastItems()
-	 */
 	@Override
 	public abstract List<ForecastItem> getForecastItems();
 
-	/* (non-Javadoc)
-	 * @see psybergate.grad2018.javafnds.finance.service.ForecastService#getInitialAmount()
-	 */
 	@Override
 	public BigDecimal getInitialAmount() {
 		return initialAmount;
 	}
 
-	/* (non-Javadoc)
-	 * @see psybergate.grad2018.javafnds.finance.service.ForecastService#setInitialAmount(java.math.BigDecimal)
-	 */
 	@Override
 	public void setInitialAmount(BigDecimal initialAmount) {
 		this.initialAmount = initialAmount;
 	}
 
-	/* (non-Javadoc)
-	 * @see psybergate.grad2018.javafnds.finance.service.ForecastService#getRate()
-	 */
 	@Override
 	public BigDecimal getRate() {
 		return rate;
 	}
 
-	/* (non-Javadoc)
-	 * @see psybergate.grad2018.javafnds.finance.service.ForecastService#setRate(java.math.BigDecimal)
-	 */
 	@Override
 	public void setRate(BigDecimal rate) {
 		this.rate = rate;
 	}
 
-	/* (non-Javadoc)
-	 * @see psybergate.grad2018.javafnds.finance.service.ForecastService#getMonths()
-	 */
 	@Override
 	public Integer getMonths() {
 		return months;
 	}
 
-	/* (non-Javadoc)
-	 * @see psybergate.grad2018.javafnds.finance.service.ForecastService#setMonths(java.lang.Integer)
-	 */
 	@Override
 	public void setMonths(Integer months) {
 		this.months = months;
